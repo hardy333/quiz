b1c18b81edd71966dc1022ac1beb979e81d8411f
gela_samsonidze <samsonidze14@gmail.com>
2024-08-07 08:15:00 +0400

    json viewer

--- src/data/quiz/quizzArr.ts
+++ src/data/quiz/quizzArr.ts
@@ -2,4 +2,33 @@ import { quiz2, quiz3 } from "./data";
 import { quizWithImg } from "./quizImg";
 import { quiz4, quiz5, quiz6, quiz7 } from "./quizzes";
 
-export const quizArr = [quiz2, quiz3, quiz4, quiz5, quiz6, quiz7, quizWithImg];
+export const quizArr = [
+  {
+    quiz: quiz2,
+    desc: "Random quiz",
+  },
+  {
+    quiz: quiz3,
+    desc: "Random Quiz",
+  },
+  {
+    quiz: quiz4,
+    desc: "quiz with only input type of questions",
+  },
+  {
+    quiz: quiz5,
+    desc: "quiz with only one choice type of questions",
+  },
+  {
+    quiz: quiz6,
+    desc: "quiz with only multy choice questions",
+  },
+  {
+    quiz: quiz7,
+    desc: "Random quiz",
+  },
+  {
+    quiz: quizWithImg,
+    desc: "quiz with images, descriptions and aditional title",
+  },
+];
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,31 +1,54 @@
 import { Link } from "react-router-dom";
 import "./home.css";
 import { quizArr } from "../../data/quiz/quizzArr";
+import { useState } from "react";
 
 const Home = () => {
+  const [quizIndex, setQuizIndex] = useState(0);
+
   return (
     <div className="container">
       {/* <QuizComponent /> */}
       <h2>Choose Quiz</h2>
       <div className="quiz-cards-container">
-        {quizArr.map((quiz, index) => {
+        {quizArr.map((item, index) => {
           return (
             <div
               className="quiz-card fade-in "
               style={{
                 animationDelay: index * 0.1 + "s",
+                border: index == quizIndex ? "2px dashed #343205" : "",
               }}
               key={index}
             >
-              <h3>{quiz.title}</h3>
+              <h3>{item.desc}</h3>
 
               <p>Lorem ipsumus hic sunt?</p>
-              <Link className="btn" to={"/quiz/" + index}>
-                Start Quiz
-              </Link>
+              <footer>
+                <Link
+                  className="btn"
+                  style={{ marginTop: "auto" }}
+                  to={"/quiz/" + index}
+                >
+                  Start Quiz
+                </Link>
+                <button
+                  onClick={() => setQuizIndex(index)}
+                  className="btn btn-success"
+                >
+                  see json
+                </button>
+              </footer>
             </div>
           );
         })}
+      </div>
+      <br />
+      <hr />
+      <br />
+
+      <div className="json-viewer">
+        <pre>{JSON.stringify(quizArr[quizIndex].quiz, null, 2)}</pre>
       </div>
     </div>
   );
